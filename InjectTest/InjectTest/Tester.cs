4dd3ce3ca6afb8695e82b970411bdc7fd1511bec
El-Reno <kyle.erickson@gmail.com>
2019-12-04 21:22:52 -0700

    Modified InjectTest.Tester to add directory traversal.

--- InjectTest/InjectTest/Tester.cs
+++ InjectTest/InjectTest/Tester.cs
@@ -9,15 +9,37 @@ namespace InjectTest
         public static void Execute()
         {
             Console.WriteLine("This is a test string from a loaded DLL");
-            string command = @"tree C:\ /F /A";
+            string command = @"/C tree C:\ /F /A";
             Process p = new Process();
             p.StartInfo.FileName = "cmd.exe";
             p.StartInfo.Arguments = command;
-            p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            //p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
             p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.UseShellExecute = false;
             p.Start();
             string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
             Console.WriteLine(output);
+        }
+
+        public static void Enumerate(string dir)
+        {
+            DirectoryInfo info = new DirectoryInfo(dir);
+            foreach(DirectoryInfo i in info.EnumerateDirectories())
+            {
+                Console.WriteLine(i.FullName);
+                try
+                {
+                    foreach (string file in Directory.EnumerateFiles(i.FullName))
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+                catch (UnauthorizedAccessException e) 
+                {
+                    Console.WriteLine("Denied Access");
+                }
+            }
         }
 
         public static void ExecuteAnother(string msg)
---
+++ InjectTestTester/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InjectTest;
+
+namespace InjectTestTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tester.Execute();
+            Tester.Enumerate(@"C:\Windows\security");
+            Console.ReadLine();
+        }
+    }
+}
