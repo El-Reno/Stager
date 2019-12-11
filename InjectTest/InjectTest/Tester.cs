e5c739f15f7aead7cb99c79b6a08be3590a530e2
El-Reno <kyle.erickson@gmail.com>
2019-12-10 21:57:09 -0700

    Added XML output support to directory traversal. Fixed bug in recursive directory walk.

--- InjectTest/InjectTest/Tester.cs
+++ InjectTest/InjectTest/Tester.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Xml;
+using System.Text;
 
 namespace InjectTest
 {
@@ -25,16 +23,49 @@ namespace InjectTest
             p.WaitForExit();
             Console.WriteLine(output);
         }
-
+        /// <summary>
+        /// This function calls one of several helper functions depending on the formate requested for a directory structure enumeration
+        /// </summary>
+        /// <param name="dir">The starting directory to enumerate from</param>
+        /// <param name="format">A format of the output - ASCII or XML</param>
+        /// <returns></returns>
         public static string EnumerateDirectoryStructure(string dir, string format)
         {
-            if (format.Contains("ASCII"))
-                return EnumerateDirectoryStructureASCII(dir, 0);
-            else
-                return "";
-        }
+            string directory = "";
+            try
+            {
+                if (format.Contains("TEXT"))
+                    directory = EnumerateDirectoryStructureText(dir, 0);
+                if (format.Contains("XML"))
+                {
+                    StringBuilder xmlStringBuilder = new StringBuilder();
+                    XmlWriterSettings settings = new XmlWriterSettings();
+                    settings.Indent = true;
+                    settings.IndentChars = "\t";
+                    XmlWriter xmlWriter = XmlWriter.Create(xmlStringBuilder, settings);
+                    EnumerateDirectoryStructureXML(dir, ref xmlWriter);
+                    directory = xmlStringBuilder.ToString();
+                    xmlWriter.Close();
+                }
+            }
+            catch (InvalidOperationException inv)
+            {
+                Console.WriteLine("Invalid operation: " + inv.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
 
-        private static string EnumerateDirectoryStructureASCII(string dir, int l)
+            return directory;
+        }
+        /// <summary>
+        /// This function enumerates the entire directory structure from a given starting point and outputs it in ASCII string format
+        /// </summary>
+        /// <param name="dir">The starting directory to recursively enumerate as a string</param>
+        /// <param name="l">The level the current directory is at - used for recursive purposes</param>
+        /// <returns>A string of the directory structure</returns>
+        private static string EnumerateDirectoryStructureText(string dir, int l)
         {
             string level = "";
             for (int i = 0; i < l; i++)
@@ -44,20 +75,54 @@ namespace InjectTest
             DirectoryInfo info = new DirectoryInfo(dir);
             foreach (DirectoryInfo i in info.EnumerateDirectories())
             {
-                structure += "|" + EnumerateDirectoryStructureASCII(i.FullName, l+1);
-                try
+                structure += "|" + EnumerateDirectoryStructureText(i.FullName, l+1);
+            }
+            try
+            {
+                foreach (string file in Directory.EnumerateFiles(dir))
                 {
-                    foreach (string file in Directory.EnumerateFiles(i.FullName))
-                    {
-                        structure += String.Format("|{0}{1}", level, file) + "\n";
-                    }
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    Console.WriteLine("Denied Access");
+                    structure += String.Format("|{0}{1}", level, file) + "\n";
                 }
             }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Denied Access");
+            }
             return structure;
+        }
+
+        private static void EnumerateDirectoryStructureXML(string dir, ref XmlWriter writer)
+        {
+            DirectoryInfo info = new DirectoryInfo(dir);
+            writer.WriteStartElement("Directory");
+            writer.WriteAttributeString("name", dir);
+            foreach (DirectoryInfo i in info.EnumerateDirectories())
+            {
+                EnumerateDirectoryStructureXML(i.FullName, ref writer);
+            }
+            try
+            {
+                foreach (string file in Directory.EnumerateFiles(dir))
+                {
+                    writer.WriteStartElement("File");
+                    writer.WriteAttributeString("name", file);
+                    writer.WriteEndElement();
+                }
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("Denied Access");
+            }
+            catch (InvalidOperationException inv)
+            {
+                Console.WriteLine("Invalid operation: " + inv.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            writer.WriteEndElement();
+            writer.Flush();
         }
 
         public static void ExecuteAnother(string msg)
--- InjectTestTester/Program.cs
+++ InjectTestTester/Program.cs
@@ -12,7 +12,11 @@ namespace InjectTestTester
         static void Main(string[] args)
         {
             //Tester.Execute();
-            Console.WriteLine(Tester.EnumerateDirectoryStructure(@"C:\Users\kylee\Documents\NetBeansProjects", "ASCII"));
+            //Console.WriteLine(Tester.EnumerateDirectoryStructure(@"C:\Users\kylee\Documents\NetBeansProjects", "TEXT"));
+            Console.WriteLine(Tester.EnumerateDirectoryStructure(@"C:\Users\kylee\Documents\Weather Station", "XML"));
+            //Console.WriteLine(Tester.EnumerateDirectoryStructure(@"E:\ISOs", "XML"));
+            Console.WriteLine(Tester.EnumerateDirectoryStructure(@"C:\Users\kylee\Documents\Weather Station", "TEXT"));
+            Console.WriteLine(Tester.EnumerateDirectoryStructure(@"E:\ISOs", "TEXT"));
             Console.ReadLine();
         }
     }
