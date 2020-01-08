9b59de0db020ebd090e3870b33ff83f189888846
Kyle Erickson <kyle.erickson@gmail.com>
2020-01-08 16:58:44 -0700

    Initial commit for Terminal.

--- Terminal/Terminal.cs
+++ Terminal/Terminal.cs
@@ -1,8 +1,49 @@
 ﻿using System;
+using Reno.Comm;
 
 namespace Reno.Stages
 {
     public class Terminal
     {
+        CommChannel channel;
+
+        /// <summary>
+        /// Constructor for the Terminal program to be placed on the client machine
+        /// </summary>
+        /// <param name="channel">Communications channel to the server</param>
+        public Terminal(CommChannel channel)
+        {
+            this.channel = channel;
+        }
+        /// <summary>
+        /// Main loop for the terminal. Loop continues to listen to commands from the server until told to disconnect
+        /// </summary>
+        public void Execute()
+        {
+            bool run = true;
+            while (run)
+            {
+                // Wait for a header - once its received execute the command
+                CommHeader header = channel.ReceiveHeader();
+            }
+        }
+
+        private void ParseCommand()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Creates a new CommHeader for transmission
+        /// </summary>
+        /// <param name="command">Command to execute</param>
+        /// <param name="compression">Compression method that is used</param>
+        /// <param name="type">Type of message: Command or Response</param>
+        /// <param name="id">Message Id</param>
+        /// <param name="data_length">Length of data</param>
+        /// <returns></returns>
+        private CommHeader CreateHeader(byte command, byte compression, byte type, int id, int data_length)
+        {
+            return new CommHeader(command, compression, type, 0, id, data_length);
+        }
     }
 }
